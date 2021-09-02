1a80bffbfe2e03a66042436f5890f6f848e5e7f7
Jon Barnumn <jonbarnum07@gmail.com>
2021-09-01 20:39:38 -0600

    js files

---
+++ app2.js
@@ -0,0 +1,9 @@
+let userAnswer = prompt('Do You Want To Go On An Adventure?');
+
+
+if (userAnswer.toLocaleLowerCase() == 'yes'); 
+
+let url = "https://24.media.tumblr.com/tumblr_m8izndR4kl1rq20pxo1_500.gif";
+
+document.write('<img src="https://24.media.tumblr.com/tumblr_m8izndR4kl1rq20pxo1_500.gif" + url + '>)
+
