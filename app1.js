6af8006374ef0f537d5c76113425abe45c790cd5
Jon Barnum <jonbarnum07@gmail.com>
2021-09-02 21:57:33 -0600

    adding pictures and guessing game

--- app1.js
+++ app1.js
@@ -17,6 +17,7 @@ function userAnswer(){
 if (userAnswer.toLowerCase() == 'yes'){
     let url = "https://24.media.tumblr.com/tumblr_m8izndR4kl1rq20pxo1_500.gif"
     document.write(`<img src="${url}">`)
+    guessingGame();
 } else {
     let url = "https://i.pinimg.com/originals/5b/7b/30/5b7b304bea8a6c105fc80a36d8025cd4.gif"
     document.write(`<img src="${url}">`)
@@ -43,27 +44,51 @@ button.addEventListener('click', function(){
     }
 });
 
+function randomNumberGenerator(){
+    let correctAnswer = Math.floor(Math.random() * 50) +1;
+    return correctAnswer;
+}
 
 
-function guessingGame (correctAnswer) {
+function guessingGame () {
     let numberOfAttempts = 8;
+    let correctAnswer = randomNumberGenerator();
+    let play = true;
+    let currentAttempt = 0;
 
-    for (let i = 1, i <= numberOfAttempts, i++); {
-        userResponse = prompt ("Guess a Number Between 1 to 50");
-
-        while ((userResponse < 1 ) || (userResponse > 50)){
+    while (play){
+        let userResponse = prompt ("Guess a Number Between 1 to 50");
+        currentAttempt = currentAttempt + 1;
+        if ((userResponse < 1 ) || (userResponse > 50)){
             userResponse = prompt ("Guess a number between 1 to 50");
-        }
-
-    if (userResponse == correctAnswer){
-        alert (Mathimatical!!!, You Got it Right!!!);
-        break;
-    } else if (userResponse < correctAnswer{
-        alert ("Supper bummer!, too low " + (numberOfAttemps - i ) + " attempts left"); `{ else if (userResponse > correctAnswer){
-                alert ("Oh No!!! too high " + (numberOfAttempts - i) + "  attempts left" );
-            }
+        } else if (userResponse == correctAnswer){
+            alert ("Mathimatical!!!, You Got it Right!!!");
+            play = false
+        } else if (userResponse < correctAnswer) {
+            alert ("Supper bummer!, too low " + (numberOfAttempts - currentAttempt ) + " attempts left"); 
+            
+        } else if (userResponse > correctAnswer){
+            alert ("Oh No!!! too high " + (numberOfAttempts - currentAttempt) + "  attempts left" );
+        }  
+        if (currentAttempt == numberOfAttempts){
+            play = false;
         }
     }
+}
 
+
+function pictureQuestion(){
+    let pictureAnswer = prompt ('How many times do you want to see a picture? Choose between 1 through 3');
+
+    console.log(pictureAnswer);
+
+    if (pictureAnswer == 1 || pictureAnswer == 2 || pictureAnswer == 3){
+        let url = "https://c.tenor.com/9JO3m3Tyj4kAAAAC/jake-adventure-time.gif";
+        for (let i = 0; i < pictureAnswer; i++){
+            document.write(`<img src="${url}">`);
+    
+        }
+    }
 }
-}
+
+pictureQuestion();
--- app3.js
+++ app3.js
@@ -33,3 +33,37 @@
 //         count = 0
 //     }
 // });
+
+
+
+
+// function userAnswer(){
+//     let userAnswer = prompt('Do You Want To Go On An Adventure?');
+
+//     console.log(userAnswer)
+
+// if (userAnswer.toLowerCase() == 'yes'){
+//     let url = "https://24.media.tumblr.com/tumblr_m8izndR4kl1rq20pxo1_500.gif"
+//     document.write(`<img src="${url}">`)
+//     guessingGame();
+// } else {
+//     let url = "https://i.pinimg.com/originals/5b/7b/30/5b7b304bea8a6c105fc80a36d8025cd4.gif"
+//     document.write(`<img src="${url}">`)
+//     }
+// };
+
+// userAnswer();
+
+
+// function pictureAnswer(){
+//     let pictureAnswer = prompt ('How many times do you want to see a picture? Choose between 1 through 3');
+
+//     console.log(pictureAnswer);
+
+//     if(pictureAnswer() == 1; 2; 3){
+//         let url = "https://i.imgur.com/Foxwa.gif";
+//         document.write(`<img src="${url}">`);
+//     }
+// }
+
+// pictureAnswer();
